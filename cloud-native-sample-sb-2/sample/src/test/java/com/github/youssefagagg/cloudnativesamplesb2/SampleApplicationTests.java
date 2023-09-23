6c3d42ed677b9ef337dfc265a1c8618e8066fb48
Youssef Agagg <yjamal@banquemisr.com>
2023-09-23 22:11:42 +0300

    refactor project structure

--- cloud-native-sample-sb-2/sample/src/test/java/com/github/youssefagagg/cloudnativesamplesb2/SampleApplicationTests.java
+++ cloud-native-sample-sb-2/sample/src/test/java/com/github/youssefagagg/cloudnativesamplesb2/SampleApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CloudNativeSampleSb2ApplicationTests {
+class SampleApplicationTests {
 
     @Test
     void contextLoads() {
