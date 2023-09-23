1a759df5dc2165ff673cdac70446747ed46dd645
Youssef Agagg <yjamal@banquemisr.com>
2023-09-23 19:44:38 +0300

    init project cloud native application with spring boot 2

---
+++ cloud-native-sample-sb-2/src/main/java/com/github/youssefagagg/cloudnativesamplesb2/CloudNativeSampleSb2Application.java
@@ -0,0 +1,13 @@
+package com.github.youssefagagg.cloudnativesamplesb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeSampleSb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNativeSampleSb2Application.class, args);
+    }
+
+}
---
+++ cloud-native-sample-sb-2/src/test/java/com/github/youssefagagg/cloudnativesamplesb2/CloudNativeSampleSb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssefagagg.cloudnativesamplesb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeSampleSb2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
