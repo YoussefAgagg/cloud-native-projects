28836b45a40cb5c000baa7ddf4fcce6d08f4f298
Youssef Agagg <yjamal@banquemisr.com>
2023-09-23 19:52:39 +0300

    expose a welcome endpoint for starting

---
+++ cloud-native-sample-sb-2/src/main/java/com/github/youssefagagg/cloudnativesamplesb2/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.github.youssefagagg.cloudnativesamplesb2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class HomeController {
+
+  @GetMapping("/")
+  public String getGreeting() {
+    return "Welcome to the cloud native !";
+  }
+}
