a7204c8fd4b5a99c3453ec0f53695bc3ea92f2ec
Youssef Agagg <yjamal@banquemisr.com>
2023-09-24 13:13:11 +0300

    add service layer

---
+++ cloud-native-sample-sb-2/book-shop/catalog-service/src/main/java/com/example/catalogservice/exception/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.catalogservice.exception;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+
+}
