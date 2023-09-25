3b2991c479d286cb048363e85e3db3563d366181
Youssef Agagg <youssefagagg@gmail.com>
2023-09-25 14:14:52 +0300

    use test containers for integration tests

---
+++ catalog-service/src/test/java/com/example/catalogservice/repository/BookRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.example.catalogservice.repository;
+
+import com.example.catalogservice.config.DataConfig;
+import com.example.catalogservice.domian.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJdbcTest
+@Import(DataConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("integration")
+class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private JdbcAggregateTemplate jdbcAggregateTemplate;
+
+    @Test
+    void findBookByIsbnWhenExisting() {
+        var bookIsbn = "1234561237";
+        var book = Book
+                .builder()
+                .isbn(bookIsbn)
+                .title("Title")
+                .author("Author")
+                .price(12.90)
+                .build();;
+        jdbcAggregateTemplate.insert(book);
+        Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
+
+        assertThat(actualBook).isPresent();
+        assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+    }
+
+}
