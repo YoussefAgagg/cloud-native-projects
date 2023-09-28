77704fb61f0e1ea39b86f7bd96dc7caff13f727e
Youssef Agagg <youssefagagg@gmail.com>
2023-09-28 15:18:23 +0300

    implement service layer

---
+++ order-service/src/main/java/com/github/youssefagagg/orderservice/service/OrderService.java
@@ -0,0 +1,45 @@
+package com.github.youssefagagg.orderservice.service;
+
+import com.github.youssefagagg.orderservice.client.BookClient;
+import com.github.youssefagagg.orderservice.client.response.Book;
+import com.github.youssefagagg.orderservice.domain.Order;
+import com.github.youssefagagg.orderservice.enums.OrderStatus;
+import com.github.youssefagagg.orderservice.repository.OrderRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+	private final BookClient bookClient;
+	private final OrderRepository orderRepository;
+
+	public OrderService(BookClient bookClient, OrderRepository orderRepository) {
+		this.bookClient = bookClient;
+		this.orderRepository = orderRepository;
+	}
+
+	public Flux<Order> getAllOrders() {
+		return orderRepository.findAll();
+	}
+
+	public Mono<Order> submitOrder(String isbn, int quantity) {
+		return bookClient.getBookByIsbn(isbn)
+				.map(book -> buildAcceptedOrder(book, quantity))
+				.defaultIfEmpty(buildRejectedOrder(isbn, quantity))
+				.flatMap(orderRepository::save);
+	}
+
+	public static Order buildAcceptedOrder(Book book, int quantity) {
+		return Order.of(book.isbn(), book.title() + " - " + book.author(),
+				book.price(), quantity, OrderStatus.ACCEPTED);
+	}
+
+	public static Order buildRejectedOrder(String bookIsbn, int quantity) {
+		return Order.of(bookIsbn, null, null, quantity, OrderStatus.REJECTED);
+	}
+
+}
