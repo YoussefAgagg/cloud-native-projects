7bb64f2ad27f89a9e1c6f51df07b1bf085694552
Youssef Agagg <youssefagagg@gmail.com>
2023-09-28 16:13:49 +0300

    fix running context load tesst

--- order-service/src/test/java/com/github/youssefagagg/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/github/youssefagagg/orderservice/OrderServiceApplicationTests.java
@@ -1,11 +1,33 @@
 package com.github.youssefagagg.orderservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class OrderServiceApplicationTests {
-
+    @Container
+    static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", OrderServiceApplicationTests::r2dbcUrl);
+        registry.add("spring.r2dbc.username", postgresql::getUsername);
+        registry.add("spring.r2dbc.password", postgresql::getPassword);
+        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
+    }
+    private static String r2dbcUrl() {
+        return String.format("r2dbc:postgresql://%s:%s/%s", postgresql.getHost(),
+                postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT), postgresql.getDatabaseName());
+    }
     @Test
     void contextLoads() {
     }
